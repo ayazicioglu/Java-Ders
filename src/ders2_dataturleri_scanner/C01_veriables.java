47953b7a3b183c5453c3eefbb44f06cc0931802d
ayazicioglu <aahmetyazicioglu@gmail.com>
2022-12-26 07:32:32 +0300

    25.12.22

---
+++ src/ders2_dataturleri_scanner/C01_veriables.java
@@ -0,0 +1,13 @@
+package ders2_dataturleri_scanner;
+
+public class C01_veriables {  public static void main(String[] args) {
+    int okulNo=885;
+    System.out.println(okulNo);
+
+    System.out.println("Okul Numarası:"+okulNo);
+
+    okulNo=899;
+    System.out.println("yeni no:"+okulNo);
+}
+}
+
---
+++ src/ders2_dataturleri_scanner/C03_nonPrimitiveDataType.java
@@ -0,0 +1,13 @@
+package ders2_dataturleri_scanner;
+
+public class C03_nonPrimitiveDataType {public static void main(String[] args) {
+    String str= "Java candir";
+    System.out.println(str.toLowerCase());
+    System.out.println(str);
+    System.out.println(str.toUpperCase());
+
+
+    System.out.println(str);
+    System.out.println(str.isEmpty());
+}
+}
